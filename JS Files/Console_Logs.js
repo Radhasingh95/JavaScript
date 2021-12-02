a20a2f269f354c7d0d89b217585adcc48f045058
Radha Singh <radha061btcsai20@igdtuw.ac.in>
2021-12-02 14:36:52 +0530

    Console Logs, Errors, Warnings

---
+++ JS Files/Console_Logs.js
@@ -0,0 +1,20 @@
+console.time('Your code took');
+console.log('Hello');
+console.log(4+34);
+console.log(true);
+console.log([1,2,3,4]);
+console.log({name:'radha',marks:99});
+console.table({name:'radha',marks:99});
+console.warn('This is a warning');
+// console.clear();
+console.timeEnd('Your code took');
+console.assert(30>189,'30 > 189 is not true');
+console.error('This is an error');
+
+// Single line comment 
+
+/*
+Multi 
+line 
+comment 
+*/
