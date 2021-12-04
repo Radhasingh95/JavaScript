957d59d294361ddbf339f8240f110c7aa307f29d
Radha Singh <radha061btcsai20@igdtuw.ac.in>
2021-12-04 18:06:23 +0530

    Window Object

---
+++ JS Files/WindowObject.js
@@ -0,0 +1,25 @@
+// DOM - Document Object Module 
+// let a = window; 
+let a = window.document; 
+// window.alert('Hello world');
+// window is a global object in javascript so we can use its method directly without writing window
+// alert('Hello world');
+
+// a = prompt('This will destroy your computer');
+
+// a = confirm('Are u sure?');
+
+a = window.innerHeight;
+a = innerWidth;
+a = scrollX;
+a = scrollY;
+a = location;
+a = location.toString();
+a = location.history;
+
+// location.reload();
+// console.log(location.href);
+// location.href = '//facebook.com';
+// history.length
+// history.go(-1)
+console.log(a)
